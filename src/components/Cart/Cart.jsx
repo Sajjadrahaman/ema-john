3359e91cf2fa491b1d3b31710a689644ec5a4ac2
Sajjadrahaman <sajjad.rahaman967@gmail.com>
2023-04-08 12:44:57 +0600

    get id from local-storage and set quantity to saveCart product and set it to SetCart also Resolve Increase quantity issue by add single item

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,13 +9,18 @@ const Cart = ({ cart }) => {
     let totalShipping = 0;
     let quantity = 0;
     for (const product of cart) {
-        // if(product.quantity === 0){
-        //     product.quantity = 1;}
-        // product.quantity = product.quantity || 1;
+        
+        //Shortcut For Quantity Issue
+        //----Option-1-----
+        /* if(product.quantity === 0){
+            product.quantity = 1;
+        } */
+        //----Option-2----
+        // product.quantity = product.quantity || 1; 
 
-        totalPrice = totalPrice + product.price;
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
-        // quantity = quantity + product.quantity;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice * 7 / 100;
     const grandTotal = totalPrice + totalShipping + tax;
@@ -23,7 +28,7 @@ const Cart = ({ cart }) => {
     return (
         <div className='cart'>
             <h4 className='CartHeader'>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -15,21 +15,20 @@ const Shop = () => {
     }, []);
 
 
-    useEffect( () =>{
+    useEffect(() => {
         const storedCart = getShoppingCart();
         const savedCart = [];
         // step 1: get id of the addedProduct
-        for(const id in storedCart){
+        for (const id in storedCart) {
             // step 2: get product from products state by using id
             const addedProduct = products.find(product => product.id === id)
-            if(addedProduct){
-                // step 3: add quantity
+            if (addedProduct) {
+                // step 3:get and set quantity
                 const quantity = storedCart[id];
                 addedProduct.quantity = quantity;
                 // step 4: add the added product to the saved cart
                 savedCart.push(addedProduct);
             }
-            // console.log('added Product', addedProduct)
         }
         // step 5: set the cart
         setCart(savedCart);
@@ -37,11 +36,29 @@ const Shop = () => {
 
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        // cart.push(product); '
+        let newCart = [];
+
+        // const newCart = [...cart, product];
+        // if product doesn't exist in the cart, then set quantity = 1
+        // if exist update quantity by 1
+
+        const exists = cart.find(pd => pd.id === product.id);
+        if (!exists) {
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else {
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
+
         setCart(newCart);
         addToDb(product.id)
     }
 
+    
     return (
         <div className='shop-container'>
             <div className='products-container'>
