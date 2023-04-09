3178920f969dde39d4e0bcfcb834bbf10fb6e2b9
Sajjadrahaman <sajjad.rahaman967@gmail.com>
2023-04-09 12:06:47 +0600

    add handleRemove function and use localstorage and filter for remove item from client side and localstorage

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import Cart from '../Cart/Cart';
+import React, { useState } from 'react';
 import './Orders.css';
-import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import { useLoaderData } from 'react-router-dom';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
-    const cart = useLoaderData();
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart)
+
+    const handleRemoveFromCart = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
+
     return (
         <div className='shop-container'>
             <div className='review-container'>
@@ -13,6 +22,7 @@ const Orders = () => {
                     cart.map(product => <ReviewItem
                         key={product.id}
                         product={product}
+                        handleRemoveFromCart={handleRemoveFromCart}
                     ></ReviewItem>)
                 }
             </div>
