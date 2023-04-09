fad354e59d9b55ae82af343f92f0d68540d99db1
Sajjadrahaman <sajjad.rahaman967@gmail.com>
2023-04-09 11:30:16 +0600

    use fontawesome on delete button

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './ReviewItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 const ReviewItem = ({ product }) => {
     const { id, img, name, price, quantity } = product;
@@ -11,7 +13,9 @@ const ReviewItem = ({ product }) => {
                 <p>Price: <span className='orange-text'>${price}</span></p>
                 <p>Order Quantity: <span className='orange-text'>{quantity}</span></p>
             </div>
-            <button className='btn-delete'>D</button>
+            <button className='btn-delete'>
+                <FontAwesomeIcon className='delete-icon' icon={faTrashAlt} />
+            </button>
         </div>
     );
 };
